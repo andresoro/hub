95d396b86dbd5d8b579a050009319bce312f1621
Andres Orozco <ao15@my.fsu.edu>
2018-10-05 20:01:06 -0400

    update example

--- examples/main.go
+++ examples/main.go
@@ -11,15 +11,18 @@ func main() {
 	hub := hub.New()
 
 	hub.NewTopic("test")
+	hub.NewTopic("test2")
+
 	c, err := hub.Subscribe("test")
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 
 	go func() {
 		for i := 0; i < 10; i++ {
 			hub.Publish("test", []byte("test"))
-			time.Sleep(2 * time.Second)
+			time.Sleep(1 * time.Second)
 		}
 		hub.Close("test")
 	}()
